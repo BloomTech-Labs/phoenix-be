96877294d43d27b2d16cc26c93ef0a6fa44ea6a7
ian-sanchez <silkyhairbear@gmail.com>
2020-02-24 20:48:16 -0800

    DB-user setup

---
+++ elephantsql.js
@@ -0,0 +1,16 @@
+var pg = require('pg');
+
+var conString = "postgres://lxdukfwc:yNm0YAijP6at_fpojLuYE2cyuCs5_BZP@raja.db.elephantsql.com:5432/lxdukfwc"
+var client = new pg.Client(conString);
+client.connect(function(err) {
+    if (err) {
+        return console.error('could not connect to postgres', err);
+    }
+    client.query('SELECT NOW() AS "theTime"', function(err, result) {
+        if(err) {
+            return console.error('error running query', err);
+        }
+        console.log(result.rows[0].theTime);
+        client.end();
+    });
+});
