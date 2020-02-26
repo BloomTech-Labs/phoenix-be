7a291bca843ce366c1898f43e2bfb216b65f7aea
ian-sanchez <silkyhairbear@gmail.com>
2020-02-25 21:03:18 -0800

    finished register and login

--- routes/auth/auth-router.js
+++ routes/auth/auth-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('./users/user-helpers.js/index.js');
+const User = require('../users/user-helpers.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -24,5 +24,24 @@ router.post('/register', (req, res) => {
             res.status(500).json({ Message: 'Error Registering User'});
         });
 });
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    console.log('outside', req.body)
+    User.getByUsername(username)
+        .then(user => {
+            console.log('inside', username)
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+                res.status(200).json({ Message: "Login Succesful!", token });
+            } else {
+                res.status(401).json({ Message: 'Error logging in' })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ Message: 'Error logging in' })
+        })
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const server = express();
 
 const usersRouter = require('./routes/users/user-router')
-
+const authRouter = require('./routes/auth/auth-router.js')
 
 server.use(express.json());
 
 server.use('/api/users', usersRouter)
-
+server.use('/auth', authRouter)
 
 
 server.get("/", (req, res) => {
