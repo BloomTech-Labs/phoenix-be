321ef0d8487977c73d5c717a566ffc9f5893859b
nantonacci <nicole.antonacci@gmail.com>
2020-03-09 19:02:10 -0700

    Added routes for calendar.

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-const express = require('express');   
+const express = require("express");
 const server = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
-const usersRouter = require('./routes/users/user-router')
-const authRouter = require('./routes/auth/auth-router.js')
-const authenticate = require('./routes/auth/auth-middleware')
-
+const usersRouter = require("./routes/users/user-router");
+const authRouter = require("./routes/auth/auth-router.js");
+const authenticate = require("./routes/auth/auth-middleware");
+const calendar = require("./routes/calendar/calendar-router");
 server.use(express.json());
-
-server.use('/api/users', authenticate, usersRouter)
-server.use('/auth', authRouter)
-
+server.use("/api/users", authenticate, usersRouter);
+server.use("/auth", authRouter);
+server.use("/calendar", calendar);
 
 server.get("/", (req, res) => {
-    res.send(`<h2>PhoeNicks!</h2>`);
-})
-
+  res.send(`<h2>PhoeNicks!</h2>`);
+});
 
 module.exports = server;
-
