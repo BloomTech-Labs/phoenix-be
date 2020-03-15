b841d3cf5fd5d92a54f14bb2e69abde998e49ec5
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-03-15 16:32:07 -0500

    import cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const cors = require('cors')
+
 const server = require("./server");
 
 const dbPort = process.env.DB_PORT;
 
 const PORT = process.env.PORT || dbPort || 4000;
-
+server.use(cors())
 server.listen(PORT, () => {
     console.log(`Server is running on localhost:${PORT}`)
 })
