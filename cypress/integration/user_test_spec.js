482ea698c42729c36fd561d89d1f490f9bc360b8
nantonacci <nicole.antonacci@gmail.com>
2020-03-19 20:45:57 -0700

    All user tests functional.

--- cypress/integration/user_test_spec.js
+++ cypress/integration/user_test_spec.js
@@ -1,47 +1,49 @@
-describe("Test user CRUD", function() {
+describe('Test user CRUD', function() {
   //post (add user)
-
-  it("Registers a user", function() {
+  var userId;
+  it('Registers a user', function() {
     cy.request({
-      method: "POST",
-      url: "/auth/register",
+      method: 'POST',
+      url: '/auth/register',
       body: {
-        username: "GuyFawkes",
-        name: "Guy Fawkes",
-        email: "guy@idea.com",
-        password: "nov05",
-        age: 450
+        username: 'JPMarat',
+        name: 'Jean Paul',
+        email: 'jp@marat.com',
+        password: 'revolution',
+        age: 277
       },
       headers: {
-        "content-type": "application/json"
+        'content-type': 'application/json'
       }
     }).then(function(response) {
-      expect(response.body).to.have.property("id");
+      expect(response.body).to.have.property('id');
+      console.log(`res body ${response.body}`);
+      userId = response.body.id[0].id;
     });
   });
   var token;
   //login
-  it("Logs in user", function() {
+  it('Logs in user', function() {
     cy.request({
-      method: "POST",
-      url: "/auth/login",
+      method: 'POST',
+      url: '/auth/login',
       body: {
-        username: "GuyFawkes",
-        password: "nov05"
+        username: 'JPMarat',
+        password: 'revolution'
       },
       headers: {
-        "content-type": "application/json"
+        'content-type': 'application/json'
       }
     }).then(function(response) {
-      expect(response.body).to.have.property("token");
+      expect(response.body).to.have.property('token');
       token = response.body.token;
     });
   });
   //get users
-  it("Gets users", function() {
+  it('Gets users', function() {
     cy.request({
-      method: "GET",
-      url: "/api/users",
+      method: 'GET',
+      url: '/api/users',
       headers: {
         Authorization: token
       }
@@ -49,9 +51,59 @@ describe("Test user CRUD", function() {
       expect(response.body).to.exist;
     });
   });
-  //get user by key
   //get user by id
+  it('Gets users by id', function() {
+    cy.request({
+      method: 'GET',
+      url: '/api/users/1',
+      headers: {
+        Authorization: token
+      }
+    }).then(function(response) {
+      expect(response.body).to.have.property('name');
+    });
+  });
 
+  //get user by key
+  it('Gets users by key', function() {
+    cy.request({
+      method: 'GET',
+      url: '/api/users/key',
+      body: {
+        name: 'Nick'
+      },
+      headers: {
+        Authorization: token,
+        'content-type': 'application/json'
+      }
+    }).then(function(response) {
+      expect(response.body).to.exist;
+    });
+  });
   //put (update user)
+  it('Updates user info', function() {
+    cy.request({
+      method: 'PUT',
+      url: `/api/users/${userId}`,
+      body: {
+        name: 'Jean-Paul'
+      },
+      headers: {
+        Authorization: token,
+        'content-type': 'application/json'
+      }
+    });
+  });
   //delete user
+  it('Deletes a user', function() {
+    cy.request({
+      method: 'DELETE',
+      url: `/api/users/${userId}`,
+      headers: {
+        Authorization: token
+      }
+    }).then(function(response) {
+      expect(response.body).to.exist;
+    });
+  });
 });
