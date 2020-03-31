5eff6c4e4ae93ff391fdb2e6e19e85028430c3b7
nantonacci <nicole.antonacci@gmail.com>
2020-03-30 20:19:22 -0700

    Registering now generates a token. A couple spelling errors fixed.

--- routes/auth/auth-router.js
+++ routes/auth/auth-router.js
@@ -2,52 +2,56 @@ const router = require('express').Router();
 const User = require('../users/user-helpers.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const cors = require('cors')
+const cors = require('cors');
 
 function generateToken(user) {
-    const payload = {
-        username: user.username,
-        id: user.id,
-    };
-    const options = {
-        expiresIn: '1d',
-    };
-    return jwt.sign(payload, process.env.JWT_SECRET, options);
+  const payload = {
+    username: user.username,
+    id: user.id
+  };
+  const options = {
+    expiresIn: '1d'
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
 }
 
-router.options('/register', cors())
+router.options('/register', cors());
 
 router.post('/register', cors(), (req, res) => {
-    let users = req.body;
-    const hash = bcrypt.hashSync(users.password, 8)
-     users.password = hash
-    
-    User.addUser(users)
-        .then(id => {
-            res.status(201).json({ Message: 'User Registeration Succesful', id });
-        })
-        .catch(err => {
-            res.status(500).json({ Message: 'Error Registering User', Error: err});
-        });
+  let users = req.body;
+  const hash = bcrypt.hashSync(users.password, 8);
+  users.password = hash;
+
+  User.addUser(users)
+    .then(id => {
+      const token = generateToken(id[0]);
+      res
+        .status(201)
+        .json({ Message: 'User Registration Successful', id, token });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ Message: 'Error Registering User', Error: err });
+    });
 });
 
-router.options('/login', cors())
+router.options('/login', cors());
 
 router.post('/login', cors(), (req, res) => {
-    const { username, password } = req.body;
-    User.getByUsername(username)
-        .then(user => {
-
-            if (user && bcrypt.compareSync(password, user.password)) {
-                const token = generateToken(user);
-                res.status(200).json({ Message: "Login Succesful!", token });
-            } else {
-                res.status(401).json({ Message: 'Error logging in', Error: err })
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ Message: 'Error logging in', Error: err })
-        })
-})
+  const { username, password } = req.body;
+  console.log(User);
+  User.getByUsername(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ Message: 'Login Successful!', token });
+      } else {
+        res.status(401).json({ Message: 'Error logging in', Error: err });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ Message: 'Error logging in', Error: err });
+    });
+});
 
 module.exports = router;
--- routes/users/user-helpers.js
+++ routes/users/user-helpers.js
@@ -1,58 +1,55 @@
-const db = require('../../database/config.js')
+const db = require('../../database/config.js');
 
 function getUsers() {
-    return db('users')
+  return db('users');
 }
 
-function getUserBy({key}) {
-    console.log('inside helper',key)
-    return db('users')
-        .where(key)
+function getUserBy({ key }) {
+  console.log('inside helper', key);
+  return db('users').where(key);
 }
 
 function getUserById(id) {
-    return db('users')
-        .where({id})
+  return db('users').where({ id });
 }
 
 function addUser(user) {
-    
-    return db
-        .insert(user)
-        .into('users')
-        .then( _=> {
-            return db('users')
-                .where({username: user.username})
-        })
+  return db
+    .insert(user)
+    .into('users')
+    .then(_ => {
+      return db('users').where({ username: user.username });
+    });
 }
 
 function updateUser(id, body) {
-    return db('users')
-        .where({id})
-        .update(body)
-        .then(res => {
-            return db('users')
-                .where({id})
-        })
+  return db('users')
+    .where({ id })
+    .update(body)
+    .then(res => {
+      return db('users').where({ id });
+    });
 }
 
 async function deleteUser(id) {
-    await db('users')
-        .where({id})
-        .del()
-        return db('users')
+  await db('users')
+    .where({ id })
+    .del();
+  return db('users');
 }
 
 function getByUsername(username) {
-    return db('users').where({ username }).first();
+  return db('users')
+    .where({ username })
+    .first();
 }
 
 module.exports = {
-    getUsers,
-    getUserBy,
-    getUserById,
-    addUser,
-    updateUser,
-    deleteUser,
-    getByUsername
-}
+  getUsers,
+  getUserBy,
+  getUserById,
+  addUser,
+  updateUser,
+  deleteUser,
+  getByUsername
+};
