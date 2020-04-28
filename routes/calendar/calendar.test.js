3763f44a4338380990283871e230953e1639d9a9
nantonacci <nicole.antonacci@gmail.com>
2020-04-27 19:10:34 -0700

    Cleaned up test for calendar get.

--- routes/calendar/calendar.test.js
+++ routes/calendar/calendar.test.js
@@ -11,19 +11,4 @@ describe('calendar testing', () => {
       expect(response.status).toBe(200);
     });
   });
-  //   describe('addEvent', () => {
-  //     it('should add a new event', async () => {
-  //       let event = await Model.addEvent({
-  //         summary: 'End of Labs Party',
-  //         location: "Emma's House",
-  //         description:
-  //           'Getting together to celebrate the successful completion of labs',
-  //         start_time: '6:00pm',
-  //         start_date: 'may 1 2020',
-  //         end_time: '10:00PM',
-  //         end_date: '2020-05-01',
-  //       });
-  //       expect(event.summary).toBe('End of Labs Party');
-  //     });
-  //   });
 });
