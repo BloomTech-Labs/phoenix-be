baf8959812013c705d0167a3b7211137e867f0cb
nantonacci <nicole.antonacci@gmail.com>
2020-04-27 21:40:17 -0700

    Attendees get by user id.

--- routes/calendar/calendar-model.js
+++ routes/calendar/calendar-model.js
@@ -1,37 +1,43 @@
-const db = require("../../database/config.js");
+const db = require('../../database/config.js');
 
 module.exports = {
   event,
   register,
+  getAttn,
   addEvent,
-  deleteEvent
+  deleteEvent,
 };
 
 //get event
 function event() {
-  return db.select("*").from("phoenixEvent as e");
+  return db.select('*').from('phoenixEvent as e');
 }
 
 //register for event
 function register(event_id, user_id) {
-  return db("attendees")
+  return db('attendees')
     .insert({ event_id, user_id })
-    .then(() => db("phoenixEvent").where({ event_id: event_id }));
+    .then(() => db('phoenixEvent').where({ event_id: event_id }));
+}
+
+//return attendees table by user id
+function getAttn(user_id) {
+  return db('attendees').then(() =>
+    db('attendees').where({ user_id: user_id })
+  );
 }
 
 //add event -ADMIN/HOST
 function addEvent(newEvent) {
   return db
     .insert(newEvent)
-    .into("phoenixEvent")
-    .then(result => {
-      return db("phoenixEvent");
+    .into('phoenixEvent')
+    .then((result) => {
+      return db('phoenixEvent');
     });
 }
 
 //delete event -ADMIN/HOST
 function deleteEvent(event_id) {
-  return db("phoenixEvent")
-    .where({ event_id })
-    .delete();
+  return db('phoenixEvent').where({ event_id }).delete();
 }
